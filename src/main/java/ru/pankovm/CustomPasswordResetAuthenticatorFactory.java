50dd8d5a9dd3eecfc80c4eae5b69bd40efc9b2d2
fourbarman <maks.java@yandex.ru>
2025-02-13 15:38:46 +0400

    init project

---
+++ src/main/java/ru/pankovm/CustomPasswordResetAuthenticatorFactory.java
@@ -0,0 +1,73 @@
+package ru.pankovm;
+
+import org.keycloak.Config;
+import org.keycloak.authentication.Authenticator;
+import org.keycloak.authentication.AuthenticatorFactory;
+import org.keycloak.models.AuthenticationExecutionModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.provider.ProviderConfigProperty;
+
+import java.util.List;
+
+public class CustomPasswordResetAuthenticatorFactory implements AuthenticatorFactory {
+    @Override
+    public String getDisplayType() {
+        return "";
+    }
+
+    @Override
+    public String getReferenceCategory() {
+        return "";
+    }
+
+    @Override
+    public boolean isConfigurable() {
+        return false;
+    }
+
+    @Override
+    public AuthenticationExecutionModel.Requirement[] getRequirementChoices() {
+        return new AuthenticationExecutionModel.Requirement[0];
+    }
+
+    @Override
+    public boolean isUserSetupAllowed() {
+        return false;
+    }
+
+    @Override
+    public String getHelpText() {
+        return "";
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return List.of();
+    }
+
+    @Override
+    public Authenticator create(KeycloakSession session) {
+        return null;
+    }
+
+    @Override
+    public void init(Config.Scope config) {
+
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory factory) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public String getId() {
+        return "";
+    }
+}
